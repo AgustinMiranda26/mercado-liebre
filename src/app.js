8de78fdc52207f1b687a279ba16a138772c021a1
AgustinMiranda26 <agumiranda3@gmail.com>
2023-11-10 21:01:22 -0300

    cambios en header y creacion de login

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ app.get('/', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.sendFile(path.join(__dirname,'views','register.html'))
 })
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,'views','login.html'))
+})
 
 app.listen(port, ()=>{
     console.log(`servidor corriendo en http://localhost${port}👌`)
